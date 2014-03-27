017b27f910e31420a7413f15fc04c9032a97e09a
chengcheukyan <chengcheukyan@hotmail.com>
2014-03-27 17:40:39 +0800

    simple ejb test client

---
+++ cityline.peter.test.ejb.client/src/main/java/cityline/peter/test/ejb/client/EJBTester.java
@@ -0,0 +1,15 @@
+package cityline.peter.test.ejb.client;
+
+public class EJBTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			RemoteEJBClient.testEJB();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
